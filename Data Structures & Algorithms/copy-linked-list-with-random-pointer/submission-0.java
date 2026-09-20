e322627ce2d9f26f2a78294da75489215a4462e6
saksham0511 <51960750+saksham0511@users.noreply.github.com>
2026-09-20 22:27:25 +0530

    Add: copy-linked-list-with-random-pointer - submission-0

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-0.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node temp = head;
+        while (temp != null) {
+            Node node = new Node(temp.val);
+            node.next = temp.next;
+            temp.next = node;
+            temp = temp.next.next;
+        }
+        temp = head;
+        while (temp != null) {
+            if (temp.random != null) {
+                temp.next.random = temp.random.next;
+            }
+            temp = temp.next.next;
+        }
+        temp = head;
+        Node head2 = temp.next;
+        while (temp != null) {
+            Node copy = temp.next;
+            temp.next = copy.next;
+            temp = temp.next;
+            if (copy.next != null) {
+                copy.next = copy.next.next;
+            }
+            copy = copy.next;
+        }
+        return head2;
+    }
+}
